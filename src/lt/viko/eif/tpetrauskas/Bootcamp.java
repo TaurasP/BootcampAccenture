f23bda698b17755c1db02f00661ff406f2660372
Tauras Petrauskas <petrauskas.post@gmail.com>
2022-03-22 19:35:43 +0200

    Added console menu, handled input exceptions.

---
+++ src/lt/viko/eif/tpetrauskas/Bootcamp.java
@@ -0,0 +1,118 @@
+package lt.viko.eif.tpetrauskas;
+
+import java.util.Scanner;
+
+public class Bootcamp {
+    Scanner scanner = new Scanner(System.in);
+    boolean exit = false;
+
+    public void start() {
+        while(!exit) {
+            showMainMenu();
+            selectFromMainMenu();
+        }
+    }
+
+    public void showMainMenu() {
+        System.out.println("\n*************** ACCENTURE BOOTCAMP ***************");
+        System.out.println("1. DAY 1");
+        System.out.println("2. DAY 2");
+        System.out.println("**************************************************");
+        System.out.println("0. EXIT");
+        System.out.println("**************************************************");
+    }
+
+    public void showDay1() {
+        Day1 day1 = new Day1();
+        day1.task1();
+    }
+
+    public void showDay2Task(int task) {
+        Day2 day2 = new Day2();
+
+        switch (task) {
+            case 1:
+                day2.task1();
+                break;
+            case 2:
+                day2.task2();
+                break;
+            case 3:
+                day2.task3();
+                break;
+            case 4:
+                day2.task4();
+                break;
+            case 5:
+                day2.task5();
+                break;
+            case 6:
+                day2.task6();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void showDay2Menu() {
+        System.out.println("\n********************* DAY 2 **********************");
+        System.out.println("1. TASK 1 (2 entered numbers comparison)");
+        System.out.println("2. TASK 2 (2 entered booleans comparison)");
+        System.out.println("3. TASK 3 (if entered number is even - true, if odd - false)");
+        System.out.println("4. TASK 4 (if entered number is odd - true, if even - false)");
+        System.out.println("5. TASK 5 (show the opposite of entered boolean)");
+        System.out.println("6. TASK 6 (if 2 entered numbers meets 1 of 3 conditions - true)");
+        System.out.println("**************************************************");
+    }
+
+    public void selectFromMainMenu() {
+        System.out.println("Select from menu: ");
+        int selection = scanner.nextInt();
+        switch (selection) {
+            case 1:
+                // DAY 1
+                showDay1();
+                break;
+            case 2:
+                // DAY 2
+                showDay2Menu();
+                selectFromDay2();
+                break;
+            case 0:
+                // EXIT
+                exit = true;
+                break;
+            default:
+                System.out.println("Error. Please enter a valid day option.");
+                break;
+        }
+    }
+
+    public void selectFromDay2() {
+        System.out.println("Select a task: ");
+        int selection = scanner.nextInt();
+        switch (selection) {
+            case 1:
+                showDay2Task(1);
+                break;
+            case 2:
+                showDay2Task(2);
+                break;
+            case 3:
+                showDay2Task(3);
+                break;
+            case 4:
+                showDay2Task(4);
+                break;
+            case 5:
+                showDay2Task(5);
+                break;
+            case 6:
+                showDay2Task(6);
+                break;
+            default:
+                System.out.println("Error. Please enter a valid task option.");
+                break;
+        }
+    }
+}
--- src/lt/viko/eif/tpetrauskas/Day2.java
+++ src/lt/viko/eif/tpetrauskas/Day2.java
@@ -1,5 +1,6 @@
 package lt.viko.eif.tpetrauskas;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Day2 {
@@ -9,62 +10,91 @@ public class Day2 {
     public void task1() {
         int a, b;
 
-        System.out.println("Enter value a: ");
-        a = scanner.nextInt();
+        try {
+            System.out.println("Enter value a (integer): ");
+            a = scanner.nextInt();
 
-        System.out.println("Enter value b: ");
-        b = scanner.nextInt();
+            System.out.println("Enter value b (integer): ");
+            b = scanner.nextInt();
 
-        System.out.println("Is " + a + " equal to " + b + "? - " + (a == b));
-        System.out.println("Is " + a + " less than " + b + "? - " + (a < b));
-        System.out.println("Is " + a + " less or equal to " + b + "? - " + (a <= b));
-        System.out.println("Is " + a + " greater than " + b + "? - " + (a > b));
-        System.out.println("Is " + a + " greater or equal to " + b + "? - " + (a >= b));
+            System.out.println("Is " + a + " equal to " + b + "? - " + (a == b));
+            System.out.println("Is " + a + " less than " + b + "? - " + (a < b));
+            System.out.println("Is " + a + " less or equal to " + b + "? - " + (a <= b));
+            System.out.println("Is " + a + " greater than " + b + "? - " + (a > b));
+            System.out.println("Is " + a + " greater or equal to " + b + "? - " + (a >= b));
+        } catch (InputMismatchException ex) {
+            System.out.println("Exception: " + ex);
+            System.out.println("Number (integer) value expected.");
+        }
     }
 
     public void task2() {
         boolean b1, b2;
 
-        System.out.println("Enter b1: ");
-        b1 = scanner.nextBoolean();
+        try {
+            System.out.println("Enter b1 (boolean): ");
+            b1 = scanner.nextBoolean();
 
-        System.out.println("Enter b2: ");
-        b2 = scanner.nextBoolean();
+            System.out.println("Enter b2 (boolean): ");
+            b2 = scanner.nextBoolean();
 
-        System.out.println("Is " + b1 + " and " + b2 + " equal? - " + (b1 == b2));
+            System.out.println("Is " + b1 + " and " + b2 + " equal? - " + (b1 == b2));
+        } catch (InputMismatchException ex) {
+            System.out.println("Exception: " + ex);
+            System.out.println("Boolean (true/false) value expected.");
+        }
     }
 
     public void task3() {
-        System.out.println("Enter a whole number (integer): ");
-        int num = scanner.nextInt();
+        try {
+            System.out.println("Enter a whole number (integer): ");
+            int num = scanner.nextInt();
 
-        System.out.println((num % 2 == 0));
+            System.out.println((num % 2 == 0));
+        } catch (InputMismatchException ex) {
+            System.out.println("Exception: " + ex);
+            System.out.println("Number (integer) value expected.");
+        }
     }
 
     public void task4() {
-        System.out.println("Enter a whole number (integer): ");
-        int num = scanner.nextInt();
+        try {
+            System.out.println("Enter a whole number (integer): ");
+            int num = scanner.nextInt();
 
-        System.out.println((num % 2 != 0));
+            System.out.println((num % 2 != 0));
+        } catch (InputMismatchException ex) {
+            System.out.println("Exception: " + ex);
+            System.out.println("Number (integer) value expected.");
+        }
     }
 
     public void task5() {
-        System.out.println("Enter boolean value: ");
-        boolean b = scanner.nextBoolean();
+        try {
+            System.out.println("Enter boolean value: ");
+            boolean b = scanner.nextBoolean();
 
-        System.out.println("Opposite of \"" + b + "\" is \"" + !b + "\"");
+            System.out.println("Opposite of \"" + b + "\" is \"" + !b + "\"");
+        } catch (InputMismatchException ex) {
+            System.out.println("Exception: " + ex);
+            System.out.println("Boolean (true/false) value expected.");
+        }
     }
 
     public void task6() {
         int a, b;
 
-        System.out.println("Enter a whole number (integer) a: ");
-        a = scanner.nextInt();
+        try {
+            System.out.println("Enter a whole number (integer) a: ");
+            a = scanner.nextInt();
 
-        System.out.println("Enter a whole number (integer) b: ");
-        b = scanner.nextInt();
+            System.out.println("Enter a whole number (integer) b: ");
+            b = scanner.nextInt();
 
-        System.out.println((a == b || a < 0 && b > 0 || a > 1001 && b > 1001));
+            System.out.println((a == b || a < 0 && b > 0 || a > 1001 && b > 1001));
+        } catch (InputMismatchException ex) {
+            System.out.println("Exception: " + ex);
+            System.out.println("Number (integer) value expected.");
+        }
     }
-
 }
